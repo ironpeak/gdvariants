4ad8b7e4eb07ac8949f731b760ba57dd46bb7914
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2022-04-03 18:26:51 +0000

    vec

--- src/vec/vec.rs
+++ src/vec/vec.rs
@@ -11,8 +11,41 @@ use std::{
     vec::{Drain, IntoIter, Splice},
 };
 
+use gdnative::{
+    core_types::VariantArray,
+    prelude::{FromVariant, FromVariantError, OwnedToVariant, ToVariant, Variant},
+};
+
 pub struct Vec<T> {
     base: std::vec::Vec<T>,
+}
+
+impl<T> FromVariant for Vec<T>
+where
+    T: FromVariant,
+{
+    fn from_variant(variant: &Variant) -> Result<Self, FromVariantError> {
+        let array = VariantArray::from_variant(variant)?;
+        let mut vec: Vec<T> = Vec::with_capacity(array.len() as usize);
+        for variant in array.iter() {
+            let value = T::from_variant(&variant)?;
+            vec.push(value);
+        }
+        Ok(vec)
+    }
+}
+
+impl<T> ToVariant for Vec<T>
+where
+    T: ToVariant,
+{
+    fn to_variant(&self) -> Variant {
+        let array = VariantArray::new();
+        for value in &self.base {
+            array.push(value.to_variant());
+        }
+        array.owned_to_variant()
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
