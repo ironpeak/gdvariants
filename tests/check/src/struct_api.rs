93855fbcba8f2de4d5079263b0f39d7d60e967d9
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2022-04-07 22:18:00 +0000

    update

--- tests/check/src/struct_api.rs
+++ tests/check/src/struct_api.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt::Display};
+use std::{collections::HashMap, fmt::Display, panic};
 
 use json::JsonValue;
 
@@ -6,6 +6,42 @@ pub struct StructApi {
     name: String,
     declaration: String,
     implementations: Vec<HashMap<String, Vec<String>>>,
+    trait_implementations: Vec<HashMap<String, Vec<String>>>,
+}
+
+fn fmt_implementations(
+    implementations: &Vec<HashMap<String, Vec<String>>>,
+    f: &mut std::fmt::Formatter<'_>,
+    ident: usize,
+) -> std::fmt::Result {
+    let indent = format!("{:ident$}", "", ident = ident);
+    write!(f, "{{")?;
+    let mut impl_delim = "";
+    for implementation in implementations {
+        for (key, methods) in implementation {
+            write!(
+                f,
+                "{}\n{indent}  \"{}\": [",
+                impl_delim,
+                key,
+                indent = indent,
+            )?;
+            let mut method_delim = "";
+            for method in methods {
+                write!(
+                    f,
+                    "{}\n{indent}    \"{}\"",
+                    method_delim,
+                    method,
+                    indent = indent,
+                )?;
+                method_delim = ",";
+            }
+            write!(f, "\n    ]")?;
+            impl_delim = ",";
+        }
+    }
+    write!(f, "\n{indent}}}", indent = indent)
 }
 
 impl Display for StructApi {
@@ -13,30 +49,39 @@ impl Display for StructApi {
         writeln!(f, "{{")?;
         writeln!(f, "  \"name\": \"{}\",", self.name)?;
         writeln!(f, "  \"declaration\": \"{}\",", self.declaration)?;
-        write!(f, "  \"implementations\": {{")?;
 
-        let mut impl_delim = "";
-        for implementation in &self.implementations {
-            for (key, methods) in implementation {
-                write!(f, "{}\n    \"{}\": [", impl_delim, key)?;
-                let mut method_delim = "";
-                for method in methods {
-                    write!(f, "{}\n      \"{}\"", method_delim, method)?;
-                    method_delim = ",";
-                }
-                write!(f, "\n    ]")?;
-                impl_delim = ",";
-            }
-        }
-        writeln!(f, "\n  }}")?;
-        writeln!(f, "}}")
+        write!(f, "  \"implementations\": ")?;
+        fmt_implementations(&self.implementations, f, 2)?;
+
+        write!(f, ",\n  \"trait_implementations\": ")?;
+        fmt_implementations(&self.trait_implementations, f, 2)?;
+
+        write!(f, "\n}}")
     }
 }
 
-fn remove_multiple_spaces(str: &str) -> String {
+fn prettify(str: &str) -> String {
     let mut previous = str.to_string();
     loop {
-        let current = previous.replace("  ", " ");
+        let current = previous
+            .replace("  ", " ")
+            .replace("&nbsp;", " ")
+            .replace("&amp;", "&")
+            .replace("&lt;", "<")
+            .replace("&gt;", ">")
+            .replace(" ::", "::")
+            .replace(":: ", "::")
+            .replace(" (", "(")
+            .replace("( ", "(")
+            .replace(" )", ")")
+            .replace(" <", "<")
+            .replace(" >", ">")
+            .replace("& ", "&")
+            .replace("? ", "?")
+            .replace(" ,", ",")
+            .replace("\n", "")
+            .trim()
+            .to_string();
         if current == previous {
             return current;
         }
@@ -50,7 +95,7 @@ fn get<'a>(json: &'a JsonValue, key: &str, value: &str) -> &'a JsonValue {
             return member;
         }
     }
-    panic!();
+    panic!("Could not find element where {} = {}", key, value);
 }
 
 fn get_with_class<'a>(json: &'a JsonValue, class: &str) -> &'a JsonValue {
@@ -59,7 +104,27 @@ fn get_with_class<'a>(json: &'a JsonValue, class: &str) -> &'a JsonValue {
             return member;
         }
     }
-    panic!();
+    panic!("Could not find element with class {}", class);
+}
+
+fn find_with_class<'a>(json: &'a JsonValue, class: &str) -> Option<&'a JsonValue> {
+    if json.is_array() {
+        for member in json.members() {
+            match find_with_class(member, class) {
+                Some(result) => return Some(result),
+                None => continue,
+            }
+        }
+    } else {
+        if json["classes"].contains(class) {
+            return Some(json);
+        }
+        if json.has_key("children") {
+            return find_with_class(&json["children"], class);
+        }
+    }
+
+    None
 }
 
 fn get_all_with_class<'a>(json: &'a JsonValue, class: &str) -> Vec<&'a JsonValue> {
@@ -78,7 +143,7 @@ fn get_with_id<'a>(json: &'a JsonValue, id: &str) -> &'a JsonValue {
             return member;
         }
     }
-    panic!();
+    panic!("Could not find element with id {}", id);
 }
 
 fn get_text<'a>(json: &'a JsonValue) -> String {
@@ -86,9 +151,9 @@ fn get_text<'a>(json: &'a JsonValue) -> String {
         let mut text = "".to_string();
         for member in json.members() {
             if member.is_string() {
-                text.push_str(member.as_str().unwrap());
+                text.push_str(format!(" {} ", member.as_str().unwrap()).as_str());
             } else {
-                text.push_str(&get_text(&member));
+                text.push_str(format!(" {} ", &get_text(&member)).as_str());
             }
         }
         text
@@ -99,12 +164,7 @@ fn get_text<'a>(json: &'a JsonValue) -> String {
             String::new()
         }
     };
-    text.replace("&nbsp;", " ")
-        .replace("&quot;", "\"")
-        .replace("&amp;", "&")
-        .replace("&lt;", "<")
-        .replace("&gt;", ">")
-        .replace("&#39;", "'")
+    text
 }
 
 fn get_name(main_content: &JsonValue) -> String {
@@ -112,7 +172,7 @@ fn get_name(main_content: &JsonValue) -> String {
         &get_with_class(&main_content["children"], "fqn")["children"],
         "in-band",
     ));
-    text[6..].to_string()
+    text.replace(" ", "")[6..].to_string()
 }
 
 fn get_declaration(main_content: &JsonValue) -> String {
@@ -120,7 +180,7 @@ fn get_declaration(main_content: &JsonValue) -> String {
         &get_with_class(&main_content["children"], "item-decl")["children"],
         "struct",
     ));
-    text.to_string()
+    prettify(&text)
 }
 
 fn get_implementation_header(implementation: &JsonValue) -> String {
@@ -130,43 +190,19 @@ fn get_implementation_header(implementation: &JsonValue) -> String {
             "impl",
         )["children"],
         "code-header",
-    ))
-    .replace(">where", "> where")
-    .replace(")where", ") where")
-    .replace("->", "-> ")
-    .replace("+", " + ")
-    .replace(":", ": ")
-    .replace(">", "> ")
-    .replace(",", ", ")
-    .trim()
-    .to_string();
-    remove_multiple_spaces(&text)
+    ));
+
+    prettify(&text)
 }
 
 fn get_implementation_methods(implementation: &JsonValue) -> Vec<String> {
     let mut methods = Vec::new();
-    for method in get_all_with_class(
-        &get_with_class(&implementation["children"], "impl-items")["children"],
-        "method-toggle",
-    ) {
-        let method = remove_multiple_spaces(&get_text(&get_with_class(
-            &get_with_class(
-                &get(&method["children"], "name", "summary")["children"],
-                "method",
-            )["children"],
-            "code-header",
-        )))
-        .replace("pub fn", "pub fn ")
-        .replace(">where", "> where")
-        .replace(")where", ") where")
-        .replace("->", "-> ")
-        .replace("+", " + ")
-        .replace("( ", "(")
-        .replace(":", ": ")
-        .replace("\n", "")
-        .trim()
-        .to_string();
-        methods.push(remove_multiple_spaces(&method));
+    for method_container in
+        get_with_class(&implementation["children"], "impl-items")["children"].members()
+    {
+        let method_element = &find_with_class(&method_container, "code-header").unwrap();
+        let method = get_text(&method_element);
+        methods.push(prettify(&method));
     }
     methods
 }
@@ -187,6 +223,25 @@ fn get_implementations(main_content: &JsonValue) -> Vec<HashMap<String, Vec<Stri
     implementations
 }
 
+fn get_trait_implementations(main_content: &JsonValue) -> Vec<HashMap<String, Vec<String>>> {
+    let mut implementations = Vec::new();
+
+    for implementation in get_all_with_class(
+        &get_with_id(&main_content["children"], "trait-implementations-list")["children"],
+        "implementors-toggle",
+    ) {
+        let trait_implementation_header = get_implementation_header(implementation);
+        let mut result = HashMap::new();
+
+        let methods = get_implementation_methods(implementation);
+
+        result.insert(trait_implementation_header, methods);
+        implementations.push(result);
+    }
+
+    implementations
+}
+
 impl StructApi {
     pub fn from_json(json: &JsonValue) -> StructApi {
         let html_element = get(&json["children"], "name", "html");
@@ -198,11 +253,13 @@ impl StructApi {
         let name = get_name(main_content);
         let declaration = get_declaration(main_content);
         let implementations = get_implementations(main_content);
+        let trait_implementations = get_trait_implementations(main_content);
 
         return StructApi {
             name: name,
             declaration: declaration,
             implementations: implementations,
+            trait_implementations: trait_implementations,
         };
     }
 }
