913627f250bdee82fdfaee4fbb08285fdbffc339
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2022-04-14 18:43:01 +0000

    update

--- src/vec/vec_impl/vec_impl_as_mut.rs
+++ src/vec/vec_impl/vec_impl_as_mut.rs
@@ -11,3 +11,28 @@ impl<T> AsMut<[T]> for Vec<T> {
         self
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_as_mut_vec() {
+        let mut stdvec = vec![2, 1, 3];
+        let mut cratevec = Vec::from(vec![2, 1, 3]);
+
+        let stdres: &std::vec::Vec<i32> = stdvec.as_mut();
+        let crateres: &Vec<i32> = cratevec.as_mut();
+
+        assert_eq!(stdres, crateres);
+    }
+
+    #[test]
+    fn test_as_mut_slice() {
+        let mut stdvec = vec![2, 1, 3];
+        let mut cratevec = Vec::from(vec![2, 1, 3]);
+
+        let stdres: &[i32] = stdvec.as_mut();
+        let crateres: &[i32] = cratevec.as_mut();
+
+        assert_eq!(stdres, crateres);
+    }
+}
