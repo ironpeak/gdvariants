28bffc74368c1b57aad8c632b314bf6aa268ebcf
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2022-04-18 20:48:46 +0000

    HashMap & HashSet (#18)

--- src/collections/hash_set_impl/hash_set_impl_bit_and.rs
+++ src/collections/hash_set_impl/hash_set_impl_bit_and.rs
@@ -38,3 +38,70 @@ where
         }
     }
 }
+
+impl<T, S> BitAnd<&std::collections::HashSet<T, S>> for &HashSet<T, S>
+where
+    T: Eq + Hash + Clone,
+    S: BuildHasher + Default,
+{
+    type Output = HashSet<T, S>;
+
+    /// Returns the intersection of `self` and `rhs` as a new `HashSet<T, S>`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use gdvariants::collections::HashSet;
+    ///
+    /// let a = HashSet::from([1, 2, 3]);
+    /// let b = HashSet::from([2, 3, 4]);
+    ///
+    /// let set = &a & &b;
+    ///
+    /// let mut i = 0;
+    /// let expected = [2, 3];
+    /// for x in &set {
+    ///     assert!(expected.contains(x));
+    ///     i += 1;
+    /// }
+    /// assert_eq!(i, expected.len());
+    /// ```
+    fn bitand(self, rhs: &std::collections::HashSet<T, S>) -> HashSet<T, S> {
+        HashSet {
+            base: self.base.bitand(rhs),
+        }
+    }
+}
+
+impl<T, S> BitAnd<&HashSet<T, S>> for &std::collections::HashSet<T, S>
+where
+    T: Eq + Hash + Clone,
+    S: BuildHasher + Default,
+{
+    type Output = HashSet<T, S>;
+    /// Returns the intersection of `self` and `rhs` as a new `HashSet<T, S>`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use gdvariants::collections::HashSet;
+    ///
+    /// let a = HashSet::from([1, 2, 3]);
+    /// let b = HashSet::from([2, 3, 4]);
+    ///
+    /// let set = &a & &b;
+    ///
+    /// let mut i = 0;
+    /// let expected = [2, 3];
+    /// for x in &set {
+    ///     assert!(expected.contains(x));
+    ///     i += 1;
+    /// }
+    /// assert_eq!(i, expected.len());
+    /// ```
+    fn bitand(self, rhs: &HashSet<T, S>) -> HashSet<T, S> {
+        HashSet {
+            base: self.bitand(&rhs.base),
+        }
+    }
+}
--- src/collections/hash_set_impl/hash_set_impl_partial_eq.rs
+++ src/collections/hash_set_impl/hash_set_impl_partial_eq.rs
@@ -11,3 +11,52 @@ where
         self.base.eq(&other.base)
     }
 }
+
+impl<T, S> PartialEq<std::collections::HashSet<T, S>> for HashSet<T, S>
+where
+    T: Eq + Hash,
+    S: BuildHasher,
+{
+    fn eq(&self, other: &std::collections::HashSet<T, S>) -> bool {
+        self.base.eq(other)
+    }
+}
+
+impl<T, S> PartialEq<HashSet<T, S>> for std::collections::HashSet<T, S>
+where
+    T: Eq + Hash,
+    S: BuildHasher,
+{
+    fn eq(&self, other: &HashSet<T, S>) -> bool {
+        self.eq(&other.base)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::collections::HashSet;
+
+    #[test]
+    fn test_eq_a() {
+        let vec_a = HashSet::from([2, 1, 3]);
+        let vec_b = HashSet::from([2, 1, 3]);
+
+        assert_eq!(vec_a, vec_b);
+    }
+
+    #[test]
+    fn test_eq_b() {
+        let vec_a = HashSet::from([2, 1, 3]);
+        let vec_b = std::collections::HashSet::from([2, 1, 3]);
+
+        assert_eq!(vec_a, vec_b);
+    }
+
+    #[test]
+    fn test_eq_c() {
+        let vec_a = std::collections::HashSet::from([2, 1, 3]);
+        let vec_b = HashSet::from([2, 1, 3]);
+
+        assert_eq!(vec_a, vec_b);
+    }
+}
