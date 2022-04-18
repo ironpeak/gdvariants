05899ef9ab3fc228619dfdf08851508cab9d54c6
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2022-04-18 20:47:59 +0000

    update

--- src/collections/hash_map_impl/hash_map_impl_partial_eq.rs
+++ src/collections/hash_map_impl/hash_map_impl_partial_eq.rs
@@ -16,3 +16,54 @@ where
         self.base.eq(&other.base)
     }
 }
+
+impl<K, V, S> PartialEq<std::collections::HashMap<K, V, S>> for HashMap<K, V, S>
+where
+    K: Eq + Hash,
+    V: PartialEq,
+    S: BuildHasher,
+{
+    fn eq(&self, other: &std::collections::HashMap<K, V, S>) -> bool {
+        self.base.eq(other)
+    }
+}
+
+impl<K, V, S> PartialEq<HashMap<K, V, S>> for std::collections::HashMap<K, V, S>
+where
+    K: Eq + Hash,
+    V: PartialEq,
+    S: BuildHasher,
+{
+    fn eq(&self, other: &HashMap<K, V, S>) -> bool {
+        self.eq(&other.base)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::collections::HashMap;
+
+    #[test]
+    fn test_eq_a() {
+        let vec_a = HashMap::from([(2, 4), (1, 2), (3, 6)]);
+        let vec_b = HashMap::from([(2, 4), (1, 2), (3, 6)]);
+
+        assert_eq!(vec_a, vec_b);
+    }
+
+    #[test]
+    fn test_eq_b() {
+        let vec_a = HashMap::from([(2, 4), (1, 2), (3, 6)]);
+        let vec_b = std::collections::HashMap::from([(2, 4), (1, 2), (3, 6)]);
+
+        assert_eq!(vec_a, vec_b);
+    }
+
+    #[test]
+    fn test_eq_c() {
+        let vec_a = std::collections::HashMap::from([(2, 4), (1, 2), (3, 6)]);
+        let vec_b = HashMap::from([(2, 4), (1, 2), (3, 6)]);
+
+        assert_eq!(vec_a, vec_b);
+    }
+}
--- src/collections/hash_set_impl/hash_set_impl_bit_or.rs
+++ src/collections/hash_set_impl/hash_set_impl_bit_or.rs
@@ -38,3 +38,71 @@ where
         }
     }
 }
+
+impl<T, S> BitOr<&std::collections::HashSet<T, S>> for &HashSet<T, S>
+where
+    T: Eq + Hash + Clone,
+    S: BuildHasher + Default,
+{
+    type Output = HashSet<T, S>;
+
+    /// Returns the union of `self` and `rhs` as a new `HashSet<T, S>`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use gdvariants::collections::HashSet;
+    ///
+    /// let a = HashSet::from([1, 2, 3]);
+    /// let b = HashSet::from([3, 4, 5]);
+    ///
+    /// let set = &a | &b;
+    ///
+    /// let mut i = 0;
+    /// let expected = [1, 2, 3, 4, 5];
+    /// for x in &set {
+    ///     assert!(expected.contains(x));
+    ///     i += 1;
+    /// }
+    /// assert_eq!(i, expected.len());
+    /// ```
+    fn bitor(self, rhs: &std::collections::HashSet<T, S>) -> HashSet<T, S> {
+        HashSet {
+            base: self.base.bitor(rhs),
+        }
+    }
+}
+
+impl<T, S> BitOr<&HashSet<T, S>> for &std::collections::HashSet<T, S>
+where
+    T: Eq + Hash + Clone,
+    S: BuildHasher + Default,
+{
+    type Output = HashSet<T, S>;
+
+    /// Returns the union of `self` and `rhs` as a new `HashSet<T, S>`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use gdvariants::collections::HashSet;
+    ///
+    /// let a = HashSet::from([1, 2, 3]);
+    /// let b = HashSet::from([3, 4, 5]);
+    ///
+    /// let set = &a | &b;
+    ///
+    /// let mut i = 0;
+    /// let expected = [1, 2, 3, 4, 5];
+    /// for x in &set {
+    ///     assert!(expected.contains(x));
+    ///     i += 1;
+    /// }
+    /// assert_eq!(i, expected.len());
+    /// ```
+    fn bitor(self, rhs: &HashSet<T, S>) -> HashSet<T, S> {
+        HashSet {
+            base: self.bitor(&rhs.base),
+        }
+    }
+}
